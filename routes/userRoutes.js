7acebe530d505c81af372c6040dd5c101e23b7f8
vfloressduoc <val.floress@duocuc.cl>
2025-05-04 18:28:07 -0400

    fix crear/editar/eliminar usuarios

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ const userController = require("../controllers/userController");
 const { protect, requireAdmin } = require("../middlewares/authMiddleware");
 
 
+
 // Todas las rutas de usuarios están protegidas
 router.use(protect);
 
@@ -12,7 +13,7 @@ router.get("/", requireAdmin, userController.getAllUsers);
 router.get("/:id", requireAdmin, userController.getUserById);
 router.post("/", requireAdmin, userController.createUser);
 router.put("/:id", requireAdmin, userController.updateUser);
-router.delete("/:id", requireAdmin, userController.deleteUser);
+router.delete("/", requireAdmin, userController.deleteUser);
 router.post("/createUserAlmacen", requireAdmin, userController.createUser);
 
 module.exports = router;
